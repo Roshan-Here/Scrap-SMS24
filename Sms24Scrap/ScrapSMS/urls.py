123955f6603d87db1772262c8fd6a138125ce54d
Muh Roshan <leoparmigiana@gmail.com>
2023-08-13 22:35:28 +0530

    added all paths + commented whattodo

---
+++ Sms24Scrap/ScrapSMS/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path,include
+
+urlpatterns = [
+    # path('<slug>/<int:pk>/',), # countrycode/number grab all code from choosen number
+    # path('<slug>/',) # to show all country code numbers 
+]
