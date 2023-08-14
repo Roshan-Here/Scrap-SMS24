bf668b4d9051d7b0ec00c3e36b70bb2af7112301
Muh Roshan <leoparmigiana@gmail.com>
2023-08-14 22:36:50 +0530

    setuped basic template

--- Sms24Scrap/ScrapSMS/Sms24Scrap/settings.py
+++ Sms24Scrap/ScrapSMS/Sms24Scrap/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMP_DIR = os.path.join(BASE_DIR,"ScrapSMS/template")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Sms24Scrap.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMP_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
